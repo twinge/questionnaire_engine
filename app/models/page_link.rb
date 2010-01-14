7ca85b6dfffc799fbab071e4aa6407342e886066
Josh Starcher <josh.starcher@gmail.com>
2010-01-14 14:40:22 -0600

    initialize all attributes

--- app/models/page_link.rb
+++ app/models/page_link.rb
@@ -4,10 +4,11 @@ class PageLink
   attr_accessor :dom_id, :label, :load_path
   attr_accessor :save_path  # to save current page
   
-  def initialize(label, load_path, dom_id)
+  def initialize(label, load_path, dom_id, save_path = nil)
     @label = label
     @load_path = load_path
     @dom_id = dom_id
+    @save_path = save_path
   end
     
 end
