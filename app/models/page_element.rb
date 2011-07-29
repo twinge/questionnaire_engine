f60e520b95c5048e836bb9f0989909dbb5aab618
Josh Starcher <josh.starcher@gmail.com>
2011-07-29 12:44:26 -0400

    better question association

--- app/models/page_element.rb
+++ app/models/page_element.rb
@@ -3,4 +3,5 @@ class PageElement < ActiveRecord::Base
   acts_as_list :scope => :page_id
   belongs_to :page
   belongs_to :element
+  belongs_to :question, :conditions => "kind NOT IN('Paragraph', 'Section', 'QuestionGrid', 'QuestionGridWithTotal')", :foreign_key => 'element_id', :class_name => 'Element'
 end
