b97fd9df5b50cc90be3030ea99593f4d65df46bc
Josh Starcher <josh.starcher@gmail.com>
2012-01-09 12:38:27 -0500

    smarter way to do letters

--- app/models/choice_field.rb
+++ app/models/choice_field.rb
@@ -120,7 +120,7 @@ class ChoiceField < Question
   end
   
   def choices_by_letter
-    letters = %w{a b c d e f g h i j k l m n o p q r s t u v w x y z}
+    letters = ('a'..'z').to_a
     choices_by_letter = Hash[choices.map.with_index {|c, i| [letters[i], c[0]]}]
   end
   
