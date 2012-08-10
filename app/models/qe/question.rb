39df2cb7bcdb4d4f79a2ce3e875995c557801ad0
Weston Platter <westonplatter@gmail.com>
2012-08-10 19:27:05 -0400

    Qe::Question was not loading due to parent class not loading.

--- app/models/qe/question.rb
+++ app/models/qe/question.rb
@@ -1,3 +1,7 @@
+# WARNING
+# without requiring Qe::Element, all inheriting models do not work
+require 'qe/element' 
+
 module Qe
   class Question < Element
     include Qe::Concerns::Models::Question
