0e42cbd07bb188e3febfb04387a89a190b4add9a
Weston Platter <westonplatter@gmail.com>
2012-08-27 10:55:44 -0400

    reference the right image

--- app/helpers/qe/application_helper.rb
+++ app/helpers/qe/application_helper.rb
@@ -43,7 +43,7 @@ module Qe
 
     def spinner(extra = nil)
       e = extra ? "spinner_#{extra}" : 'spinner'
-      image_tag('qe/spinner.gif', :id => e, :style => 'display:none', :class => 'spinner')
+      image_tag('qe/status.gif', :id => e, :style => 'display:none', :class => 'spinner')
     end
 
   end
