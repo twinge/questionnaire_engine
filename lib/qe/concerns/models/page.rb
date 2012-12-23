5c685ff08377872a57e87cab818fcd0cb8748503
Weston Platter <westonplatter@gmail.com>
2012-12-23 15:47:50 -0600

    need to explicitly declare -- class_name -- foreign_key
    I believe this is because is an affect of the Rails Engine namespacing.
    Specs pass with these explicit declarations.

--- lib/qe/concerns/models/page.rb
+++ lib/qe/concerns/models/page.rb
@@ -6,6 +6,15 @@ module Qe
         extend ActiveSupport::Concern
 
         included do
+          has_many :qe_page_elements,
+            :class_name => Qe::PageElement,
+            :foreign_key => :qe_page_id
+
+          has_many :qe_elements,
+            :through => :qe_page_elements,
+            :dependent => :destroy,
+            :order => :position
+
           belongs_to :qe_question_sheet
         end
 
