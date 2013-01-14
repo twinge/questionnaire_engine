5063ee5438044a8a0159e3f2071f453dc49c07c5
Weston Platter <westonplatter@gmail.com>
2013-01-14 00:05:29 -0600

    updated to rails 3.2.11 and removed concern files

--- app/controllers/qe/application_controller.rb
+++
@@ -1,4 +0,0 @@
-module Qe
-  class ApplicationController < ActionController::Base
-  end
-end
--- lib/qe.rb
+++ lib/qe.rb
@@ -1,14 +1,5 @@
 require 'qe/engine'
 
-require 'qe/concerns/models/question_sheet'
-require 'qe/concerns/models/page_element'
-require 'qe/concerns/models/page'
-require 'qe/concerns/models/element'
-
-require 'qe/concerns/models/answer_sheet'
-require 'qe/concerns/models/answer'
-require 'qe/concerns/models/condition'
-
 module Qe
 end
 
