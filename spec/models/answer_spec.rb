3374521760788ccd4a539d30226d172b949a6c7d
Weston Platter <westonplatter@gmail.com>
2013-02-19 08:51:03 -0600

    closes #10 - remove db specs after columns changed

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,14 +4,6 @@ describe Qe::Answer do
   it { should have_db_column :answer_sheet_id }
   it { should have_db_column(:value) }
   it { should have_db_column(:short_value) }
-  it { should have_db_column(:size) }
-  it { should have_db_column(:content_type) }
-  it { should have_db_column(:filename) }
-  it { should have_db_column(:height) }
-  it { should have_db_column(:width) }
-  it { should have_db_column(:parent_id) }
-  it { should have_db_column(:thumbnail) }
-  it { should have_db_index :short_value }
   
   it { should belong_to :answer_sheet }
   it { should belong_to :question }
