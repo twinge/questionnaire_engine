1b2d757ed877d0e26cedef08cf40f7dd4af9018a
Weston Platter <westonplatter@gmail.com>
2013-03-17 17:55:29 -0500

    added missing end

--- db/migrate/20130124080523_create_qe_reference_sheets.rb
+++ db/migrate/20130124080523_create_qe_reference_sheets.rb
@@ -4,21 +4,22 @@ class CreateQeReferenceSheets < ActiveRecord::Migration
     if table_exists?(:reference_sheets)
       rename_table(:reference_sheets, Qe::QuestionSheet.table_name)
     else
-    create_table Qe::ReferenceSheet.table_name do |t|
-      t.integer :question_id
-      t.integer :applicant_answer_sheet_id
-      t.datetime :email_sent_at
-      t.string :relationship
-      t.string :title
-      t.string :first_name
-      t.string :last_name
-      t.string :phone
-      t.string :email
-      t.string :status
-      t.datetime :submitted_at
-      t.string :access_key
-    
-      t.timestamps
+      create_table Qe::ReferenceSheet.table_name do |t|
+        t.integer :question_id
+        t.integer :applicant_answer_sheet_id
+        t.datetime :email_sent_at
+        t.string :relationship
+        t.string :title
+        t.string :first_name
+        t.string :last_name
+        t.string :phone
+        t.string :email
+        t.string :status
+        t.datetime :submitted_at
+        t.string :access_key
+      
+        t.timestamps
+      end
     end
     add_column Qe::Element.table_name, :question_sheet_id, :integer
   end
