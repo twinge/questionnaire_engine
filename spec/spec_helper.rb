4ad98729c1bc8dcbf7d857834898d36fa6396cc2
Weston Platter <westonplatter@gmail.com>
2013-03-18 13:29:57 -0500

    nuke capybara and add shoulda

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,10 @@
-require 'simplecov'
-if ENV["COVERAGE"]
-  SimpleCov.start 'rails' do
-    add_filter 'vendor'
-  end
-end
-
 ENV["RAILS_ENV"] ||= 'test'
 
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require 'rspec/rails'
-require 'capybara/rspec'
 require 'factory_girl_rails'
 require 'database_cleaner'
+require 'shoulda'
 
 ENGINE_RAILS_ROOT = File.join( File.dirname(__FILE__), '../' )
 Dir[File.join(ENGINE_RAILS_ROOT,"spec/support/**/*.rb")].each {|f| require f}
