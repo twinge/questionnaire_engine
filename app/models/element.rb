03eb1a73069827a5d27782c60340e3212af05680
Josh Starcher <josh.starcher@gmail.com>
2013-05-10 17:33:16 -0400

    Fix bug with deleting elements
    Deleting an element would mess up the order of elements in a grid

--- app/models/element.rb
+++ app/models/element.rb
@@ -2,66 +2,76 @@
 class Element < ActiveRecord::Base
   set_table_name "#{Questionnaire.table_name_prefix}#{self.table_name}"
   belongs_to :question_grid, :class_name => "QuestionGrid", :foreign_key => "question_grid_id"
-  
+
   self.inheritance_column = :kind
-  
+
   belongs_to :page
   belongs_to :question_sheet
-  
-  acts_as_list :scope => :page_id
+
+  acts_as_list
 
   validates_presence_of :kind
   # validates_presence_of :label, :style, :on => :update
-  
+
   validates_length_of :kind, :style, :maximum => 40, :allow_nil => true
   #validates_length_of :label, :maximum => 255, :allow_nil => true
 
   # TODO: This needs to get abstracted out to a CustomQuestion class in BOAT
   validates_inclusion_of :kind, :in => %w{Section Paragraph TextField ChoiceField DateField FileField SchoolPicker ProjectPreference StateChooser QuestionGrid QuestionGridWithTotal AttachmentField}  # leaf classes
-  
+
   before_create :set_defaults
-  
+
   HUMANIZED_ATTRIBUTES = {
     :slug => "Variable"
   }
-    
+
   def self.human_attribute_name(attr)
     HUMANIZED_ATTRIBUTES[attr.to_sym] || super
   end
-  
-  
+
+
   def question?
     self.kind_of?(Question)
   end
-  
-  
+
+
   # by default the partial for an element matches the class name (override as necessary)
   def template
     self.class.to_s.underscore
   end
-  
+
+  def scope_condition
+    condition = "page_id = #{page_id}"
+    if question_grid_id
+      condition += " AND question_grid_id = #{question_grid_id}"
+    else
+      condition += " AND question_grid_id is NULL"
+    end
+    condition
+  end
+
   # copy an item and all it's children
   def duplicate(grid_id = nil)
     new_element = self.class.new(self.attributes)
     new_element.question_grid_id = grid_id if grid_id
     new_element.save!
-    
+
     # duplicate children
     if respond_to?(:elements) && elements.present?
       elements.each {|e| e.duplicate(new_element.id)}
     end
-    
+
     new_element
   end
-  
+
   protected
   def set_defaults
     self.label = "Untitled" if label.nil?
     if self.content.nil?
       case self.class.to_s
         when "ChoiceField" then self.content = "Choice One\nChoice Two\nChoice Three"
-        when "Paragraph" then self.content ="Lorem ipsum..." 
-      end 
+        when "Paragraph" then self.content ="Lorem ipsum..."
+      end
     end
 
     if self.style.nil?
@@ -75,8 +85,8 @@ class Element < ActiveRecord::Base
         when "SchoolPicker" then self.style = "school_picker"
         when "ProjectPreference" then self.style = "project_preference"
         when "StateChooser" then self.style = "state_chooser"
-      end 
+      end
     end
   end
-    
+
 end
