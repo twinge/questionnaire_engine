d14883276d8d6c9fb5908302407945fde1d63e3a
Weston Platter <westonplatter@gmail.com>
2013-05-10 23:38:41 -0500

    [controllers spec] email_templates_controller passing. added factories.

--- spec/controllers/admin/email_templates_controller_spec.rb
+++ spec/controllers/admin/email_templates_controller_spec.rb
@@ -3,45 +3,45 @@ require 'spec_helper'
 describe Qe::Admin::EmailTemplatesController do
 
 	before(:each) do
-		@template = Qe::EmailTemplate.create!(name: 'test')
+		@template = FactoryGirl.create(:email_template)
 	end
 	
 	it 'GET index' do
-		get :index,
-			use_route: 'qe'
+		get :index, use_route: 'qe'
 	end
+
 	it 'GET new' do
-		get :new,
-			use_route: 'qe'
+		get :new, use_route: 'qe'
 	end
+	
 	it 'GET edit' do
-		get :edit,
-			use_route: 'qe',
+		get :edit, use_route: 'qe',
 			id: @template.id
 	end
+
 	it 'POST create' do
 		name = "post_create"
 		attributes = Qe::EmailTemplate.new(name: name).attributes
 		
-		post :create,
-			use_route: 'qe',
+		post :create, use_route: 'qe',
 			email_template: attributes
 
 		created = Qe::EmailTemplate.find_by_name(name)
 		created.should_not == nil
 	end
+
 	it 'PUT update' do
 		name = 'put_update'
 		@template.name = name
 
-		put :update,
-			use_route: 'qe',
+		put :update, use_route: 'qe',
 			id: @template.id,
 			email_template: @template.attributes
 
 		@updated_template = Qe::EmailTemplate.find(@template.id)
 		@updated_template.name.should == name
 	end
+	
 	it 'DELETE destroy' do
 		delete :destroy,
 			use_route: 'qe',
---
+++ spec/support/factories/email_templates.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do 
+  
+  factory :email_template, class: Qe::EmailTemplate do 
+    name 'template name'
+  end
+  
+end
