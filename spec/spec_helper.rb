0526e87298a88f5ca211e13a89dcfce4b5663971
Weston Platter <westonplatter@gmail.com>
2013-05-11 00:03:47 -0500

    [specs] change the rspec runtime configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,22 @@
-ENV["RAILS_ENV"] ||= 'test'
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 
+ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require 'rspec/rails'
-require 'factory_girl_rails'
-require 'database_cleaner'
+require 'rspec/autorun'
 require 'shoulda'
+require 'database_cleaner'
+require 'factory_girl_rails'
 
 ENGINE_RAILS_ROOT = File.join( File.dirname(__FILE__), '../' )
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 Dir[File.join(ENGINE_RAILS_ROOT,"spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
 
-  config.color_enabled = true
-  config.include FactoryGirl::Syntax::Methods
-
-  # Remove models from database before and after test suite is run
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
@@ -26,11 +28,15 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
+  # muted to allow database_cleaner to work
+  #
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
+  # set to true (embrace the future)
+  #
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
@@ -40,6 +46,5 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  # config.order = "36080"
   config.order = "random"
 end
