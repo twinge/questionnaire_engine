527ca6d509cf2e976e8a16fb9438b3a6b30c4b41
Josh Starcher <josh.starcher@gmail.com>
2013-07-25 16:52:19 -0600

    Freeze frozen fields

--- app/models/question_set.rb
+++ app/models/question_set.rb
@@ -1,36 +1,37 @@
 # QuestionSet
 # represents a group of elements, with their answers
 class QuestionSet
-  
+
   attr_reader :elements
-  
+
   # associate answers from database with a set of elements
   def initialize(elements, answer_sheet)
       @elements = elements
       @answer_sheet = answer_sheet
-      
+
       @questions = elements.select { |e| e.question? }
-      
+
       answers = @answer_sheet.answers_by_question
-      
+
       @questions.each do |question|
         question.answers = answers[question.id]
-      end    
+      end
       @questions
   end
-  
+
   # update with responses from form
   def post(params)
     questions_indexed = @questions.index_by {|q| q.id}
-    
+
     # loop over form values
     params ||= {}
     params.each do |question_id, response|
       # update each question with the posted response
-      questions_indexed[question_id.to_i].response = posted_values(response)
+      question = questions_indexed[question_id.to_i]
+      question.response = posted_values(response) if question
     end
   end
-  
+
   def valid?
     valid = true
     @questions.each do |question|
@@ -38,11 +39,11 @@ class QuestionSet
     end
     valid
   end
-  
+
   def any_questions?
     @questions.length > 0
   end
-  
+
   def save
     AnswerSheet.transaction do
       @questions.each do |question|
@@ -50,13 +51,13 @@ class QuestionSet
       end
     end
   end
-  
-  
+
+
   private
-  
+
   # convert posted response to a question into Array of values
   def posted_values(param)
-  
+
     if param.kind_of?(Hash) and param.has_key?('year') and param.has_key?('month')
       year = param['year']
       month = param['month']
@@ -71,9 +72,9 @@ class QuestionSet
     elsif param.kind_of?(String)
       values = [CGI.unescape(param)]
     end
-  
+
     # Hash may contain empty string to force post for no checkboxes
 #    values = values.reject {|r| r == ''}
   end
-  
+
 end
