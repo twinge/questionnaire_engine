92e40c7c7f3b9e663358bc9acb52bd0945ab1aff
Weston Platter <westonplatter@gmail.com>
2013-09-27 18:16:45 -0600

    remove spec/dummy from version control. not sure why this is back in

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '782cd48bdf6a9ce4f32963aa904b9a419ec4f93c29218af0516ea273bc4ec2e76c3ef640af44c38a22fcf023b335071d93c904182ae2a45bf102274f99973d54'
