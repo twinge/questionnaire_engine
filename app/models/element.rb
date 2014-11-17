11e40b644f829337ad5c6880df8d89b6b7b5c571
Josh Starcher <josh.starcher@gmail.com>
2014-11-17 13:40:42 -0500

    Make sure all elements respond to the response methods

--- app/models/element.rb
+++ app/models/element.rb
@@ -3,43 +3,55 @@ class Element < ActiveRecord::Base
   set_table_name "#{Questionnaire.table_name_prefix}#{self.table_name}"
   belongs_to :question_grid, :class_name => "QuestionGrid", :foreign_key => "question_grid_id"
   belongs_to :choice_field, :class_name => "ChoiceField", :foreign_key => "conditional_id"
-  
+
   self.inheritance_column = :kind
-  
+
   has_many :page_elements, :dependent => :destroy
   has_many :pages, :through => :page_elements
-  
+
   scope :active, select("distinct(#{Questionnaire.table_name_prefix}elements.id), #{Questionnaire.table_name_prefix}elements.*").where(QuestionSheet.table_name + '.archived' => false).joins({:pages => :question_sheet})
-  
+
   # belongs_to :question_sheet
 
   validates_presence_of :kind, :style
   # validates_presence_of :label, :style, :on => :update
-  
+
   validates_length_of :kind, :style, :maximum => 40, :allow_nil => true
   # validates_length_of :label, :maximum => 255, :allow_nil => true
 
   # TODO: This needs to get abstracted out to a CustomQuestion class in BOAT
   validates_inclusion_of :kind, :in => %w{Section Paragraph TextField ChoiceField DateField FileField SchoolPicker ProjectPreference StateChooser QuestionGrid QuestionGridWithTotal AttachmentField ReferenceQuestion PaymentQuestion}  # leaf classes
-  
+
   before_validation :set_defaults, :on => :create
-  
+
   # HUMANIZED_ATTRIBUTES = {
   #   :slug => "Variable"
   # }
-  #   
+  #
   # def self.human_attrib_name(attr)
   #   HUMANIZED_ATTRIBUTES[attr.to_sym] || super
   # end
-  
+
   def has_response?(answer_sheet = nil)
     false
   end
-  
+
+  def response(app)
+    ''
+  end
+
+  def responses(app)
+    []
+  end
+
+  def display_response(app)
+    ''
+  end
+
   def required?(answer_sheet = nil)
     super()
   end
-  
+
   def position(page = nil)
     if page
       page_elements.where(:page_id => page.id).first.try(:position)
@@ -47,7 +59,7 @@ class Element < ActiveRecord::Base
       self[:position]
     end
   end
-  
+
   def set_position(position, page = nil)
     if page
       pe = page_elements.where(:page_id => page.id).first
@@ -57,17 +69,17 @@ class Element < ActiveRecord::Base
     end
     position
   end
-  
+
   def question?
     self.kind_of?(Question)
   end
-  
-  
+
+
   # by default the partial for an element matches the class name (override as necessary)
   def ptemplate
     self.class.to_s.underscore
   end
-  
+
   # copy an item and all it's children
   def duplicate(page, parent = nil)
     new_element = self.class.new(self.attributes)
@@ -79,15 +91,15 @@ class Element < ActiveRecord::Base
     end
     new_element.save(:validate => false)
     PageElement.create(:element => new_element, :page => page) unless parent
-    
+
     # duplicate children
     if respond_to?(:elements) && elements.present?
       elements.each {|e| e.duplicate(page, new_element)}
     end
-    
+
     new_element
   end
-  
+
   # include nested elements
   def all_elements
     if respond_to?(:elements)
@@ -96,11 +108,11 @@ class Element < ActiveRecord::Base
       []
     end
   end
-  
+
   def reuseable?
     (self.is_a?(Question) || self.is_a?(QuestionGrid) || self.is_a?(QuestionGridWithTotal))
   end
-  
+
   def Element.max_label_length
     @@max_label_length ||= Element.columns.find{ |c| c.name == "label" }.limit
   end
@@ -110,8 +122,8 @@ class Element < ActiveRecord::Base
     if self.content.blank?
       case self.class.to_s
         when "ChoiceField" then self.content ||= "Choice One\nChoice Two\nChoice Three"
-        when "Paragraph" then self.content ||="Lorem ipsum..." 
-      end 
+        when "Paragraph" then self.content ||="Lorem ipsum..."
+      end
     end
 
     if self.style.blank?
@@ -130,8 +142,8 @@ class Element < ActiveRecord::Base
       when "ReferenceQuestion" then self.style ||= "peer"
       else
         self.style ||= self.class.to_s.underscore
-      end 
+      end
     end
   end
-    
+
 end
